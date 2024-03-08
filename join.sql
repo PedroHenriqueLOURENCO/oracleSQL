2afdc7ddff4bd3619b3e7105283c253e84cc4f0e
pedrohenriquelourenco <rm98402@fiap.com.br>
2024-03-08 20:54:54 -0300

    exercicio join

--- join.sql
+++ join.sql
@@ -1,6 +1,20 @@
 --criar um bloco pl/sql que traga o endereço completo do cliente e o codigo do cliente 
 
-select a.nom_pais pais, b.nom_estado estado, c.nom_cidade cidade, d.des_bairro , d.num_endereco endereco_cliente
+set SERVEROUTPUT on
+declare
+v_pais pais.nom_pais%type;
+v_estado estado.nom_estado%type;
+v_cidade cidade.nom_cidade%type;
+v_bairro endereco_cliente.des_bairro%type;
+v_endereco_cliente endereco_cliente.num_endereco%type;
+v_cliente cliente.cod_cliente%type;
+begin
+select a.nom_pais,
+b.nom_estado ,
+c.nom_cidade ,
+d.des_bairro ,
+d.num_endereco ,
+e.cod_cliente into v_pais, v_estado, v_cidade,v_bairro, v_endereco_cliente, v_cliente
 from pais a
 inner join estado b
 on (a.cod_pais=b.cod_pais)
@@ -8,4 +22,9 @@ inner join cidade c
 on (b.cod_estado=c.cod_estado)
 inner join endereco_cliente d
 on (c.cod_cidade=d.cod_cidade)
+inner JOIN cliente e
+on (d.cod_cliente=e.cod_cliente)
 where a.cod_pais = 4;
+dbms_output.put_line ('o codigo de cliente é ' ||v_cliente);
+dbms_output.put_line ('endereço cliente é ' ||v_pais || v_estado);
+end;
